5aa5082ad70695fd94c3eda0acf5d63eb6bb6ef3
park-kook <89983499+park-kook@users.noreply.github.com>
2022-05-26 08:08:40 -0400

    Update 43-Multiply-Strings.py

--- 43-Multiply-Strings.py
+++ 43-Multiply-Strings.py
@@ -4,13 +4,13 @@ class Solution:
             return "0"
         
         res = [0] * (len(num1) + len(num2))
-        num1, num2 = num1[::-1], num2[::-1]
+        num1, num2 = num1[::-1], num2[::-1] #reversed
         for i1 in range(len(num1)):
             for i2 in range(len(num2)):
                 digit = int(num1[i1]) * int(num2[i2])
                 res[i1 + i2] += digit
-                res[i1 + i2 + 1] += res[i1 + i2] // 10
-                res[i1 + i2] = res[i1 + i2] % 10
+                res[i1 + i2 + 1] += res[i1 + i2] // 10 #if 18 then 1 is saved in res[+1]
+                res[i1 + i2] = res[i1 + i2] % 10 # if 8 saved in res[0]
         
         res, beg = res[::-1], 0
         while beg < len(res) and res[beg] == 0:
